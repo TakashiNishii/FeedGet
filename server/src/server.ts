a0c94e4daefc898e2e0de7a03926d420f98d5177
Takashi <ygortn14@gmail.com>
2022-05-08 23:02:02 -0400

    :bug: Mudando a migraçäo do DB para PostgreeSQL

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,8 @@ app.use(express.json()); //use json na nossa API
 app.use(routes) //use as rotas que estão no routes.ts
 
 
+app.set('port', process.env.PORT || 3333); //seta a porta que será usada
 
-app.listen(process.env.PORT || 3333, () => {
+app.listen( app.get('port'), () => {
     console.log("HTTP server running!");
 }); //listen é uma função do express que faz o servidor ouvir a porta 3333
